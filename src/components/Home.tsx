686f75ae3460aacbc2982ad9fea863c613b7ff1b
bisubus <subus.gh@gmail.com>
2019-08-10 09:58:23 +0300

    Add modular CSS example

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import classnames from 'classnames';
 
-import styles from './Home.css';
+import './Home.css';
+import styles from './Home.module.css';
 
 export const Home: React.FC = () => (
-	<div className={classnames(styles.Home)}>
+	<div className={classnames('Home', styles.Home)}>
 		<p>Edit <code>Home.tsx</code> and save to reload.</p>
 	</div>
 );
